d48b09eaf28538a9878c5e25175e86a100277bf2
ScnuWang <scnu_wang@163.com>
2018-07-23 17:11:41 +0800

    整合APScheduler模块实现定时抓取功能

---
+++ geekparity/run.py
@@ -0,0 +1,28 @@
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+from scrapy.utils.project import get_project_settings
+from geekparity.spiders.xiaomi_spider import XiaomiSpider
+from geekparity.spiders.wangyi_spider import WangyiSpider
+
+from apscheduler.schedulers.twisted import TwistedScheduler
+
+configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
+
+def crawlxiaomi():
+    runner = CrawlerRunner(get_project_settings())
+    runner.crawl(XiaomiSpider)
+    runner.crawl(WangyiSpider)
+    d = runner.join()
+    d.addBoth(lambda _: reactor.stop())
+
+
+if __name__ == '__main__':
+    sched = TwistedScheduler()
+    sched.add_job(crawlxiaomi,'cron',hour=17,minute=10)
+    sched.start()
+
+    try:
+        reactor.run()
+    except Exception:
+        pass
